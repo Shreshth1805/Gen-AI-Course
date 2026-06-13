a96a8de3ac5c5891faba419cd67fc5d71c505c60
Shreshth1805 <shreshthverma1805@gmail.com>
2026-06-13 16:28:30 +0530

    chatbots and Vector

---
+++ Langchain/LLM LCEL/serve.py
@@ -0,0 +1,44 @@
+import os
+import langserve
+from fastapi import FastAPI
+from langserve import add_routes
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key=groq_api_key)
+
+## Chat Templates:
+generic_template="translate the following data into {language}:"
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system",generic_template),
+        ("user","{text}")
+    ]
+)
+
+parser=StrOutputParser()
+
+## Creating chain:
+chain=prompt|model|parser
+
+## App Definition:
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using langchain runnable")
+## Adding chain route:
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
+
+
+
